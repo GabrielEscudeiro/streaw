f73986a33400b7a00fa22740ab21913279e203fd
Gabriel Escudeiro <gabrielescudeiro1@gmail.com>
2021-06-16 01:36:24 -0300

    Header e footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view />
+    <AppHeader />
+    <main>
+      <router-view />
+    </main>
+    <AppFooter />
   </div>
 </template>
 
-<style></style>
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+main {
+  flex: 1;
+}
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  padding: 0px;
+  margin: 0px;
+}
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: Helvetica, Arial, sans-serif;
+  color: #345;
+  background: url("./assets/pattern.svg") repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  background: rgb(86, 179, 40);
+  color: white;
+  padding: 10px 30px;
+  font-size: 1rem;
+  font-family: Helvetica, Arial, sans-serif;
+  text-align: center;
+  border-radius: 4px;
+  box-shadow: 0 4px 8px rgba(18, 190, 41, 0.2);
+  transition: all 0.3s;
+  border: none;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: rgb(64, 133, 30);
+  transform: scale(1.1);
+}
+</style>
